db3e4b774fd86af9fff686ec7c332eec9ffd1c0f
johnnyd21 <jdominguez@richmondhillhs.org>
2020-10-17 17:35:04 +0000

    Final Atlas map()

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import data from './atlas-of-remote-islands.js';
 
 function App() {
   console.log(data);
+  let island1 = data.islands[0]
+  console.log(island1.description);
   
   return (
     <div>
@@ -16,8 +18,26 @@ function App() {
         <h2 className="section-title">Islands</h2>
         <ul className="cards">
           
-          <Card />
-          
+           {data.islands.map(island => {
+               return(<Card name={island.name}   
+                description={island.description}
+                image={island.image}
+                latitude={island.coords.lat}
+                longitude={island.coords.lon}/>)
+
+           })}
+
+          <Card name={island1.name}   
+                description={island1.description}
+                image={island1.image}
+                latitude={island1.coords.lat}
+                longitude={island1.coords.lon}
+            
+                 />
+
+                
+
+
         </ul>
       </div>
     </div>
